a763eff519d934fafe3f1d990b8c60ba3d88465b
duanzhiwei <armchannel_dzw@163.com>
2017-07-26 14:22:20 +0800

    Initial Commit

---
+++ DeviceInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeviceInfo
+//
+//  Created by dzw on 2017/7/26.
+//  Copyright © 2017年 段志巍. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeviceInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeviceInfo
+//
+//  Created by dzw on 2017/7/26.
+//  Copyright © 2017年 段志巍. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
